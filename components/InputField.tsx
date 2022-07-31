eb1aa614ff72eaa4cc11048d8a0f86a130583755
mmaleszewski <mmaleszewski@trans.eu>
2022-07-31 19:31:47 +0200

    chore: update input form component customization

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useFormContext } from "react-hook-form";
 
+type InputType = "text" | "number" | "date";
 type Props = {
   label: string;
   name: string;
-  type?: "text" | "number" | "date";
+  type?: InputType;
   className?: string;
+  step?: string;
 };
 
-const InputField = ({ label, type = "text", name, className }: Props) => {
+const InputField = ({
+  label,
+  type = "text",
+  name,
+  className,
+  step = "1",
+}: Props) => {
   const { register } = useFormContext();
+  const configInput = useMemo(() => {
+    switch (type) {
+      case "date":
+        return {};
+      case "number":
+        return {
+          placeholder: "number",
+          defaultValue: 0,
+          step,
+        };
+
+      default:
+        return {};
+    }
+  }, [type, step]);
   return (
     <div className={`relative ${className}`}>
       <label className="text-gray-700">
         {label}
+
         <input
-          {...register(name)}
-          step=".01"
+          {...register(name, {
+            valueAsNumber: type === "number",
+          })}
           type={type}
+          {...configInput}
           className="rounded-lg
           border-transparent
           flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-          placeholder="number"
         />
       </label>
     </div>
