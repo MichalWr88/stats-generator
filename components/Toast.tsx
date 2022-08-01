828f41ceaa426d37ec594922f9ced3b5e0d703c3
mmaleszewski <mmaleszewski@trans.eu>
2022-08-01 08:35:12 +0200

    chore: add color type

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,18 +1,17 @@
 import React from "react";
 import { Toaster } from "react-hot-toast";
 import resolveConfig from "tailwindcss/resolveConfig";
+import { DefaultColors } from "tailwindcss/types/generated/colors.js";
 import tailwindConfig from "../tailwind.config.js";
 // import { TailwindConfig, TailwindValues } from "tailwindcss/tailwind-config";
 
+type keyColorProps = "50" | "100" | "200" | "300" | '400' | "500" | "600" | '700'| "800" | "900";
 const fullConfig = resolveConfig(tailwindConfig);
-const colors = fullConfig?.theme?.colors as { red: { 500: string } };
+const colors = fullConfig?.theme?.colors as unknown as DefaultColors;
 
-type Props = {
-  type?: "succes" | "error" | "normal" | "warning";
-};
+
 
 const Toast = () => {
-  console.dir(colors);
   return (
     <Toaster
       toastOptions={{
