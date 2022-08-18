260a9a8dc2068bbb0bafde6d3477ad80d0b5787a
mmaleszewski <mmaleszewski@trans.eu>
2022-08-18 13:15:09 +0200

    chore: add version to front

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Image from 'next/image';
-
+import { version } from '../package.json';
 const Footer = () => {
   return (
-    <div className="bg-gray-500 self-stretch flex justify-center text-white p-2">
+    <div className="bg-gray-500 self-stretch flex justify-around text-white p-2">
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
@@ -14,6 +14,7 @@ const Footer = () => {
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </span>
       </a>
+      <div className="flex justify-end items-end w-1/2">v{version}</div>
     </div>
   );
 };
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,8 +5,16 @@ import NavLink from './NavLink';
 
 const NavBar = () => {
   return (
-    <header className="bg-gray-500 flex items-center p-2 self-stretch">
-      <nav className="flex-1 flex justify-around p-2">
+    <header
+      className="bg-gray-800 flex items-center p-2 self-stretch 
+    shadow-lg
+  navbar navbar-expand-lg 
+    "
+    >
+      <nav
+        className="flex-1 flex justify-around p-2  shadow-lg
+  navbar navbar-expand-lg "
+      >
         {navBarData.map((link) => {
           return <NavLink link={link} key={link.id} />;
         })}
