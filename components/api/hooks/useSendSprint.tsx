c2ef94c38ae07e089c5d41a0da788a03315c57e6
mmaleszewski <mmaleszewski@trans.eu>
2022-09-01 12:49:30 +0200

    feat: edit issues in add form

--- components/api/hooks/useSendSprint.tsx
+++ components/api/hooks/useSendSprint.tsx
@@ -2,10 +2,15 @@ import { Sprint } from '@/models/Sprint';
 import { useMutation, UseMutateFunction } from '@tanstack/react-query';
 import { sendSprintData } from '../dataProvider';
 
-const useSendSprint = (): UseMutateFunction<Sprint, unknown, Sprint, unknown> => {
-  const { mutate } = useMutation(['send-sprint'], (data: Sprint) => sendSprintData(data));
+interface MutationProps {
+  mutate: UseMutateFunction<Sprint, unknown, Sprint, unknown>;
+  isSuccess: boolean;
+}
 
-  return mutate;
+const useSendSprint = (): MutationProps => {
+  const { mutate, isSuccess } = useMutation(['send-sprint'], (data: Sprint) => sendSprintData(data));
+
+  return { mutate, isSuccess };
 };
 
 export default useSendSprint;
--- pages/add-sprint.tsx
+++ pages/add-sprint.tsx
@@ -1,63 +1,127 @@
+import useGetAppConfig from '@/components/api/hooks/useGetAppConfig';
+import SelectField from '@/components/SelectField';
 import SprintForm from '@/components/SprintForm';
 import Table from '@/components/table/Table';
 import UploadFile from '@/components/UploadFile';
+import { allImoGroups } from '@/data/epicGroups';
 import { Issue } from '@/models/Sprint';
 import WithNavBar from 'layouts/WithNavBar';
 import React, { useState, useMemo } from 'react';
 import { Column } from 'react-table';
 
 const AddSprintPage = () => {
+  const { data: epicList = [] } = useGetAppConfig('epic');
+
   const [data, setData] = useState<Array<Issue>>([]);
+  const updateMyData = (value: string, rowIndex: number, columnId: string) => {
+    setData((old) =>
+      old.map((row, index) => {
+        if (index === rowIndex) {
+          return {
+            ...old[rowIndex],
+            [columnId]: value,
+          };
+        }
+        return row;
+      })
+    );
+  };
   const columns: Array<Column<Issue>> = useMemo(
     () => [
       {
-        Header: 'IssueKey',
-        accessor: 'IssueKey',
+        Header: 'Issues',
+        columns: [
+          {
+            Header: 'IssueKey',
+            accessor: 'IssueKey',
+          },
+          {
+            Header: 'EpicGroup',
+            accessor: 'EpicGroup',
+            Cell: (cell) => {
+              return (
+                <div
+                  className={`p-2 ${
+                    (cell.row.original.Typeofwork === 'Organization' && cell.row.original.EpicGroup) ||
+                    (cell.row.original.Typeofwork !== 'Organization' && !cell.row.original.EpicGroup)
+                      ? 'bg-red-500'
+                      : ''
+                  }`}
+                >
+                  <SelectField
+                    options={epicList.map((epic) => epic.name)}
+                    initValue={cell.row.original.EpicGroup}
+                    updateMyData={(value) => updateMyData(value, cell.row.index, cell.column.id)}
+                  />
+                </div>
+              );
+            },
+          },
+          {
+            Header: 'Typeofwork',
+            accessor: 'Typeofwork',
+            Cell: (cell) => {
+              return (
+                <SelectField
+                  options={Object.keys(allImoGroups)}
+                  initValue={cell.row.original.Typeofwork}
+                  updateMyData={(value) => updateMyData(value, cell.row.index, cell.column.id)}
+                />
+              );
+            },
+          },
+          {
+            Header: 'Issuesummary',
+            accessor: 'Issuesummary',
+          },
+          {
+            Header: 'Hours',
+            accessor: 'Hours',
+          },
+          {
+            Header: 'IssueType',
+            accessor: 'IssueType',
+          },
+          {
+            Header: 'EpicLink',
+            accessor: 'EpicLink',
+          },
+          {
+            Header: 'Username',
+            accessor: 'Username',
+          },
+          {
+            Header: 'WorkDescription',
+            accessor: 'WorkDescription',
+          },
+          {
+            Header: 'ParentKey',
+            accessor: 'ParentKey',
+          },
+        ],
+      },
+      // {
+      //   Header: 'Typeofwork',
+      //   columns: [
+      //     {
+      //       Header: '',
+      //       accessor: 'Typeofwork',
+      //       Cell: (cell: { row: { original: Issue } }) => {
+      //         return (
+      //           <SelectField
+      //             options={Object.keys(allImoGroups)}
+      //             initValue={cell.row.original.EpicGroup}
+      //             updateMyData={(value) => updateMyData(value, cell.row)}
+      //           />
+      //         );
+      //       },
+      //     },
+      //   ],
 
-        // className: "p-2 flex bg-gray-900 text-white w-200",
-      },
-      {
-        Header: 'EpicGroup',
-        accessor: 'EpicGroup',
-
-        // className: "p-2 flex bg-gray-900 text-white w-200",
-      },
-      {
-        Header: 'Typeofwork',
-        accessor: 'Typeofwork',
-
-        // className: "p-2 flex bg-gray-900 text-white w-200",
-      },
-      {
-        Header: 'Issuesummary',
-        accessor: 'Issuesummary',
-      },
-      {
-        Header: 'Hours',
-        accessor: 'Hours',
-      },
-      {
-        Header: 'IssueType',
-        accessor: 'IssueType',
-      },
-      {
-        Header: 'EpicLink',
-        accessor: 'EpicLink',
-      },
-      {
-        Header: 'Username',
-        accessor: 'Username',
-      },
-      {
-        Header: 'WorkDescription',
-        accessor: 'WorkDescription',
-      },
-      {
-        Header: 'ParentKey',
-        accessor: 'ParentKey',
-      },
+      //   // className: "p-2 flex bg-gray-900 text-white w-200",
+      // },
     ],
-    []
+    [epicList]
   );
 
   const addFile = (arr: Array<Issue>) => {
