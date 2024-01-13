0ef52fe9f6c6e1c8785a2381cf57c584b2ac8648
mmaleszewski <mmaleszewski@trans.eu>
2024-01-13 08:27:58 +0100

    bump to next13

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    typedRoutes: true,
+    mdxRs: true,
+    serverActions: true,
+  },
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
 
 module.exports = nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./layouts/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/UI/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {},
