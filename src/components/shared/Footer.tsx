3aec9b4d3d0e2ee5446b88164ff56b702adb6a65
mmaleszewski <mmaleszewski@trans.eu>
2024-01-23 15:05:29 +0100

    Refactor Footer component styles

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -3,18 +3,19 @@ import Image from 'next/image';
 import config from '../../../package.json';
 const Footer = () => {
   return (
-    <div className="bg-gray-500 self-stretch flex justify-around text-white p-2">
+    <div className="flex self-stretch justify-around p-2 text-white bg-gray-500">
       <a
         href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
         target="_blank"
         rel="noopener noreferrer"
+        className='flex items-center gap-2'
       >
         Powered by{' '}
         <span className="">
           <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} data-testid="img" />
         </span>
       </a>
-      <div className="flex justify-end items-end w-1/2" data-testid="version">
+      <div className="flex items-end justify-end w-1/2" data-testid="version">
         v{config.version}
       </div>
     </div>
