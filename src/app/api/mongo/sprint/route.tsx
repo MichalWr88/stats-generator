68b3ee3aea60a0189cc71e1a3e0c9ff710e203e1
mmaleszewski <mmaleszewski@trans.eu>
2024-01-26 17:12:28 +0100

    Add API routes for editing and adding sprints

---
+++ src/app/api/mongo/sprint/route.tsx
@@ -0,0 +1,32 @@
+import { MongoServerError } from 'mongodb';
+import { NextResponse } from 'next/server';
+import { type Sprint, sprintSchemaEdit, sprintSchemaAdd } from '@/models/Sprint';
+import { mongoSprint } from '@/mongoService';
+
+export async function PUT(request: Request) {
+  const body = (await request.json()) as Sprint;
+  try {
+    await sprintSchemaEdit.validate(body);
+    const resp = await mongoSprint.editOne(body);
+    return NextResponse.json(resp, { status: 200 });
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as Sprint;
+  try {
+    await sprintSchemaAdd.validate(body);
+    const resp = await mongoSprint.addOne(body);
+    return NextResponse.json(resp, { status: 200 });
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ error: error }, { status: 500 });
+  }
+}
