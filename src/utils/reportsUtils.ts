22810558bde598a6f1ebf9acdf64dfb85681b63f
mmaleszewski <mmaleszewski@trans.eu>
2024-01-26 17:17:15 +0100

    Refactor imports and make download functions asynchronous

--- src/utils/reportsUtils.ts
+++ src/utils/reportsUtils.ts
@@ -1,27 +1,27 @@
-import { SprintWithStats } from 'src/models/Sprint';
-import { downloadAllSprintsCSV, downloadIssuesCSV } from '@/components/api/dataProvider';
-import { parseLocalDate } from 'src/utils';
+import { downloadIssuesCSV, downloadAllSprintsCSV } from '@/api/dataProvider';
+import { type SprintWithStats } from '@/models/Sprint';
+import { parseLocalDate } from '.';
 
-export const getIssueCSV = (sprint: SprintWithStats) => {
-  downloadIssuesCSV(Number(sprint.nr)).then((data) => {
+export const getIssueCSV = async (sprint: SprintWithStats) => {
+  await downloadIssuesCSV(Number(sprint.nr)).then((data) => {
     const url = window.URL.createObjectURL(new Blob([data]));
     const link = document.createElement('a');
     link.href = url;
     link.setAttribute(
       'download',
-      sprint.nr +
+      sprint.nr.toString() +
         ' ' +
-        parseLocalDate(new Date(sprint.start || new Date())) +
+        parseLocalDate(new Date(sprint.start || new Date())).toString() +
         '-' +
-        parseLocalDate(new Date(sprint.end || new Date()))
+        parseLocalDate(new Date(sprint.end || new Date())).toString()
     );
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   });
 };
-export const getAllSprintsCSV = () => {
-  downloadAllSprintsCSV().then((data) => {
+export const getAllSprintsCSV = async () => {
+  await downloadAllSprintsCSV().then((data) => {
     const url = window.URL.createObjectURL(new Blob([data]));
     const link = document.createElement('a');
     link.href = url;
