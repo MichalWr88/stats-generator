48ade1b2bda45e0dfe21553bdfc6807854db3e9b
mmaleszewski <mmaleszewski@trans.eu>
2024-01-26 17:17:05 +0100

    Refactor imports and remove commented code

--- src/mongoService/queryHelpers.ts
+++ src/mongoService/queryHelpers.ts
@@ -1,86 +1,10 @@
-import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, PaginationResponse, PaginationResponseAggregate } from 'src/models/mongo/Mongo';
-import { PipelineStage } from 'mongoose';
-
-// interface AggrSort {
-//   $sort: {
-//     [key: string]: -1 | 1;
-//   };
-// }
-// interface DateRangeSearch {
-//   $and: [EndDateSearch, StarDateSearch] | [] | [EndDateSearch] | [StarDateSearch];
-// }
-
-// enum DateProps {
-//   SERVER = 'serverDate',
-//   CREATED = 'createdAt',
-// }
-
-// type StarDateSearch = {
-//   [key in DateProps]?: {
-//     $gte: Date;
-//   };
-// };
-// type EndDateSearch = {
-//   [key in DateProps]?: {
-//     $lte: Date;
-//   };
-// };
-
-// export const setSortObj = (
-//   param: Pick<SearchParams, "sortBy" | "sortDir">
-// ): AggrSort => {
-//   const { sortBy = "nr", sortDir = 1 } = param || {
-//     sortBy: "serverDate",
-//     sortDir: 1,
-//   };
-//   return {
-//     $sort: {
-//       [sortBy]: sortDir,
-//     },
-//   };
-// };
-
-// export const dateRangeBlock = (
-//   props: Pick<SearchParams, "startDate" | "endDate">
-// ): DateRangeSearch | undefined => {
-//   const { startDate, endDate } = props || { startDate: null, endDate: null };
-
-//   if (!startDate && !endDate) {
-//     return;
-//   }
-//   const dateRangeQuery: DateRangeSearch = {
-//     $and: [],
-//   };
-
-//   if (startDate) {
-//     const sIso = new Date(startDate);
-//     sIso.setHours(0);
-//     sIso.setMinutes(0);
-//     sIso.setSeconds(0);
-//     sIso.setMilliseconds(0);
-//     const gteObj: StarDateSearch = {
-//       ["serverDate"]: {
-//         $gte: sIso,
-//       },
-//     };
-//     dateRangeQuery.$and = [...(dateRangeQuery.$and as []), gteObj];
-//   }
-//   if (endDate) {
-//     const eIso = new Date(endDate);
-//     eIso.setHours(23);
-//     eIso.setMinutes(59);
-//     eIso.setSeconds(59);
-//     eIso.setMilliseconds(999);
-//     const lteObj: EndDateSearch = {
-//       ["serverDate"]: {
-//         $lte: eIso,
-//       },
-//     };
-//     dateRangeQuery.$and = [...(dateRangeQuery.$and as []), lteObj];
-//   }
-
-//   return dateRangeQuery;
-// };
+import { type PipelineStage } from 'mongoose';
+import {
+  DEFAULT_PAGE,
+  DEFAULT_PAGE_SIZE,
+  type PaginationResponseAggregate,
+  type PaginationResponse,
+} from '@/models/mongo/Mongo';
 
 export const queryPagination = (page = DEFAULT_PAGE, pageSize = DEFAULT_PAGE_SIZE): PipelineStage[] => {
   return [
