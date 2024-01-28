2d0409ad775d2a183dfb256f3c1d3e4912ee37bb
mmaleszewski <mmaleszewski@trans.eu>
2024-01-28 08:31:11 +0100

    Update MongoDB connection and add new environment variable add MONGO_DB_NAME

--- src/mongoService/index.tsx
+++ src/mongoService/index.tsx
@@ -1,48 +1,52 @@
-
-import { connect, connection } from 'mongoose';
-import MongoConfig from './mongoConfig';
+import { type MongoError } from 'mongodb';
+import { type ConnectOptions, connect, connection } from 'mongoose';
+import { checkMongoConnected } from '@/utils/mongoHelpers';
+import MongoEvent from './MongoEvents';
 import MongoSprint from './MongoSprint';
 import MongoUser from './MongoUsers';
-import MongoEvent from './MongoEvents';
-import { checkMongoConnected } from '@/utils/mongoHelpers';
+import MongoConfig from './mongoConfig';
 
-// import MongoUser from "./MongoUser";
-// import MongoCompany from "./MongoWork";
+const mongodbOptions: ConnectOptions = {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // strictQuery: true
+  // user: process.env.MONGO_USER,
+  // pass: process.env.MONGO_PASSWORD,
+};
 
-export const initConnectMongo = (): Promise<boolean> => {
+export const initConnectMongo = async () => {
   try {
-    // if (process.env.PRODUCTION) {
-    //   set("debug", true);
-    // }
     console.log('initConnectMongo');
-    return new Promise((resolve, reject) => {
-      const mongodbOptions = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        user: process.env.MONGO_USER,
-        pass: process.env.MONGO_PASSWORD,
-      };
-      connect(
-        `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/trans?retryWrites=true&w=majority`,
-        mongodbOptions
-      );
-      connection.once('open', () => {
-        console.log('✅ Connected to DB');
-        resolve(true);
-      });
-      connection.on('error', (error) => {
-        console.log(`❌ Error on DB Connection:${error}`);
-        reject(error);
-        if (!checkMongoConnected()) {
-          console.log(`❌ Error on DB Connection:${error.message}`);
-        }
-      });
+    if (
+      !process.env.MONGO_USER ||
+      !process.env.MONGO_PASSWORD ||
+      !process.env.MONGO_CLUSTER ||
+      !process.env.MONGO_DB_NAME
+    ) {
+      const errorMessage = 'MONGO_USER or MONGO_PASSWORD or MONGO_CLUSTER or MONGO_DB_NAME not set';
+      console.log('❌ ' + errorMessage);
+      return new Error(errorMessage);
+    }
+    connection.once('open', () => {
+      console.log(`✅ Connected to DB  ${process.env.MONGO_DB_NAME ?? 'unknown'}`);
     });
-  } catch (err) {
+    connection.on('error', (error: string) => {
+      console.log(`❌ Error on DB Connection:${error}`);
+      if (!checkMongoConnected()) {
+        const err = error as unknown as MongoError;
+        console.log(`❌ Error on DB Connection:${err.message}`);
+      }
+    });
+    return await connect(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`,
+      mongodbOptions
+    );
+  } catch (err: unknown) {
+    console.log(err);
     throw err;
   }
 };
-initConnectMongo();
+await initConnectMongo();
 export const mongoSprint = new MongoSprint();
 export const mongoConfig = new MongoConfig();
 export const mongoUser = new MongoUser();
