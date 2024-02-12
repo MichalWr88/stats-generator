efe0b338f355a43fe6283789681bd14a16a1a14b
mmaleszewski <mmaleszewski@trans.eu>
2024-02-12 09:11:55 +0100

    Refactor ErrorBoundary component in ReactBoundry.tsx

--- src/utils/ReactBoundry.tsx
+++ src/utils/ReactBoundry.tsx
@@ -1,32 +1,35 @@
-"use client";
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-// @ts-ignore
-import React, { Component } from 'react';
+import React, { Component, type ErrorInfo, type ReactNode } from "react";
 
-export class ErrorBoundary extends Component {
-  // @ts-ignore
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
-  // @ts-ignore
-  static getDerivedStateFromError() {
+interface Props {
+  children?: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  public static getDerivedStateFromError(_: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true };
   }
-  // @ts-ignore
-  componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    console.log(error, errorInfo);
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
   }
 
-  render() {
-    // @ts-ignore
+  public render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      return <h1>Sorry.. there was an error</h1>;
     }
-    // @ts-ignore
+
     return this.props.children;
   }
 }
+
+export default ErrorBoundary;
