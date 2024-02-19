6ba90646c7217420946c5b8795187c2573e9f3b8
mmaleszewski <mmaleszewski@trans.eu>
2024-02-19 17:47:55 +0100

    Update Button component styling and add disabled state handling

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -7,9 +7,10 @@ type Props = {
   clickHandle: (event: MouseEvent<HTMLButtonElement>) => void;
   type?: 'outline' | 'bold';
   rest?: object;
+  disabled?: boolean;
 };
 const primaryClassName =
-  'h-8 w-full my-3 p-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out';
+  'h-8 w-full my-3 p-2.5 bg-blue-600 text-gray-600 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out';
 
 const getClassByType = (type: 'outline' | 'bold' | undefined) => {
   switch (type) {
@@ -26,9 +27,13 @@ const Button = (props: Props) => {
   const { className = '', children, clickHandle, type, rest = {} } = props;
   return (
     <button
-      className={`${primaryClassName} ${getClassByType(type)} ${className}`}
+      className={`${primaryClassName} ${getClassByType(type)} ${className} ${
+        !props.disabled
+          ? ''
+          : 'cursor-not-allowed bg-gray-400 text-gray-800 hover:bg-gray-400 border-gray-400 focus:bg-gray-400 focus:shadow-none focus:border-gray-400 active:bg-gray-400 active:shadow-none active:border-gray-400 transition-none ease-none'
+      }`}
       type="button"
-      onClick={clickHandle}
+      onClick={() => (!props.disabled ? clickHandle : null)}
       {...rest}
     >
       {children}
