a37c541f49a4737a25a400482eaa7ffd9e899a6c
mmaleszewski <mmaleszewski@trans.eu>
2024-02-26 10:05:42 +0100

    Update next.config.js and react-hook-form version

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
   experimental: {
     typedRoutes: true,
     mdxRs: true,
-    serverActions: true,
+    // serverActions: true,
   },
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
 };
